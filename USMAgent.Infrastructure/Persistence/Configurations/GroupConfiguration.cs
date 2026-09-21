99f13dbf9364077a9d30db873589218b4b3860f2
nadejda-statova <statovanadejda04@gmail.com>
2026-09-21 21:46:53 +0300

    feat: add entity configurations

---
+++ USMAgent.Infrastructure/Persistence/Configurations/GroupConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace USMAgent.Infrastructure.Persistence.Configurations;
+public class GroupConfiguration : IEntityTypeConfiguration<Group>
+{
+    public void Configure(EntityTypeBuilder<Group> builder)
+    {
+        builder.HasKey(g => g.Id);
+        builder.Property(g => g.Name).HasMaxLength(32).IsRequired();
+        builder.Property(g => g.Language).HasMaxLength(16).IsRequired();
+        builder.HasIndex(g => g.Name).IsUnique();
+    }
+}
+

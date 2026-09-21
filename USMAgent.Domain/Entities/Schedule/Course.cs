2f97ccba37ec82a935eca648681df34d78b377c5
nadejda-statova <statovanadejda04@gmail.com>
2026-09-21 23:52:52 +0300

    chore: move files

---
+++ USMAgent.Domain/Entities/Schedule/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace USMAgent.Domain.Entities.Schedule;
+public class Course
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public ICollection<CourseAlias> Aliases { get; set; } = [];
+
+    public ICollection<ScheduleEntry> ScheduleEntries { get; set; } = [];
+}
